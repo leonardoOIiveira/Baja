356829e61718ec3122a088b1836856dddd6c3110
leonerd42 <leosoliveira10@gmail.com>
2020-03-01 17:32:31 -0300

    Alterado o caminho do servidor

--- .history/src/app/core/services/config.service_20200229212327.ts
+++ .history/src/app/core/services/config.service_20200229212327.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class ConfigService {
 
-  private baseLink: string = 'http://ec2-3-133-83-196.us-east-2.compute.amazonaws.com:3000';
+  private baseLink: string = 'http://127.0.0.1:3000';
   private login: string = '/api/auth/login';
   private eventos: string = '/api/events';
   private usuario: string = '/api/users/';
